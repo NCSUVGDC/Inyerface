192b9b71845ecfc82f3ec598e8aff11fbf916cf1
Griffin Geiger <griffin2420@yahoo.com>
2020-04-04 13:33:58 -0400

    Made Level generation scene and default tiles

---
+++ Inyerface_project/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum ArenaType
+{
+    Hub,
+    T,
+    Straight,
+    Bend
+}
+
+
+//Each tile type has a canonical orientation. The orientation is the way that the
+//Canonical orientation arrow is pointing
+public enum Orientation
+{
+    North,
+    East,
+    South,
+    West
+}
