39a180d73521fab0a8df88c0265a7dfa2e718852
Brady Geiger <bradycole18@gmail.com>
2020-04-04 23:37:08 -0400

    Character controller and item pickup

---
+++ Inyerface_project/Assets/PinwheelFantasyEffect/Script/Fireball.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Fireball : MonoBehaviour {
+    public bool pushOnAwake = true;
+    public Vector3 startDirection;
+    public float startMagnitude;
+    public ForceMode forceMode;
+
+    public GameObject fieryEffect;
+    public GameObject smokeEffect;
+    public GameObject explodeEffect;
+
+    protected Rigidbody rgbd;
+
+    public void Awake()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        rgbd.Sleep();
+        if (fieryEffect != null)
+        {
+            StopParticleSystem(fieryEffect);
+        }
+        if (smokeEffect != null)
+        {
+            StopParticleSystem(smokeEffect);
+        }
+        if (explodeEffect != null)
+            explodeEffect.SetActive(true);
+    }
+
+    public void StopParticleSystem(GameObject g)
+    {
+        ParticleSystem[] par;
+        par = g.GetComponentsInChildren<ParticleSystem>();
+        foreach(ParticleSystem p in par)
+        {
+            p.Stop();
+        }
+    }
+
+    public void OnEnable()
+    {
+       if (fieryEffect != null)
+            fieryEffect.SetActive(true);
+        if (smokeEffect != null)
+            smokeEffect.SetActive(true);
+        if (explodeEffect != null)
+            explodeEffect.SetActive(false);
+         
+    }
+}
+
+
